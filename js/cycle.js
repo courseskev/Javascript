b7f57b89c02368cdaf115ecb53d841112a54611b
Jachk0011 <kibarra@unal.edu.co>
2022-04-17 18:48:19 -0500

    updating structure of files to use multiples html according with the task related.

--- js/cycle.js
+++ js/cycle.js
@@ -1,4 +1,3 @@
-//alert();
 
 //Cycles and iterations activity
 //Idea: Request a word/number different to "ESC/esc" and these will be appear in a list.
@@ -6,6 +5,7 @@
 
 let word = prompt("Enter a text to create a list of words. Use ESC to end the list.");
 let num = 1;
+console.log("List of words:")
 while (word.toLowerCase() != "esc") {        
     console.log(num++ +") " + word);        
     word = prompt("Enter a text to create a list of words. Use ESC to end the list.");
